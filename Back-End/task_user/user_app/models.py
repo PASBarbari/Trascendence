ead1b82ea4acaf824d7ca1a9e773253b06f5adeb
DOPEgduranti <guglielmo.duranti10@gmail.com>
2024-11-20 16:33:25 +0100

    default=1

--- Back-End/task_user/user_app/models.py
+++ Back-End/task_user/user_app/models.py
@@ -5,8 +5,8 @@ import psycopg2
 class Avatars(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=255)
-    # image = models.ImageField(upload_to="avatar/", null=True)
 	image = models.URLField(max_length=200, default='https://drive.google.com/file/d/1MDi_OPO_HtWyKTmI_35GQ4KjA7uh0Z9U/view?usp=drive_link')
+    # image = models.ImageField(upload_to="avatar/", null=True)
 
 class Users(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,7 +18,7 @@ class Users(models.Model):
     bio = models.TextField(default="")
     exp = models.IntegerField(default=0)
     level = models.IntegerField(default=0)
-    avatar = models.ForeignKey(Avatars, on_delete=models.SET(1), related_name="picture")
+    avatar = models.ForeignKey(Avatars, default=1, on_delete=models.SET(1), related_name="picture")
     last_modified = models.DateTimeField(auto_now=True)
 
 class Friendships(models.Model):
