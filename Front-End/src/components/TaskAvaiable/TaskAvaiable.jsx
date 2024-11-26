4820f9c41041226a6d0fa3240cd4fb46383f2480
sim1gariglio <simone.gariglio1@gmail.com>
2024-11-26 09:49:15 -0500

    Tasks component and TaskAvailable

--- Front-End/src/components/TaskAvaiable/TaskAvaiable.jsx
+++ Front-End/src/components/TaskAvaiable/TaskAvaiable.jsx
@@ -1,13 +1,28 @@
 import "./TaskAvaiable.css";
 import React, { useEffect, useState } from "react";
 import { getCookie } from "../Cookie.jsx";
-
-{
-  /* //task disponibili a cui ti puoi iscrivere per categoria  */
-}
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Avatar, ListItemAvatar } from "@mui/material";
+import Task from "../Task/Task.jsx";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 
 export default function TaskAvaiable() {
   const [tasks, setTask] = useState([]);
+  const [value, setValue] = React.useState("SP");
+  const tabListRef = React.useRef(null);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const filteredTasks = tasks.filter((task) => task.category === value);
 
   const handleGetTasks = async () => {
     try {
@@ -16,7 +31,7 @@ export default function TaskAvaiable() {
         headers: {
           "Content-Type": "application/json",
           "X-CSRFToken": getCookie("csrftoken"),
-          "Authorization": `Bearer ${localStorage.getItem("token")}`,
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
 
@@ -33,25 +48,91 @@ export default function TaskAvaiable() {
     }
   };
 
-	useEffect(() => {
-		handleGetTasks();
-	}, []);
+  useEffect(() => {
+    handleGetTasks();
+  }, []);
+
+  useEffect(() => {
+    const selectedTab = tabListRef.current.querySelector(
+      `[aria-selected="true"]`
+    );
+    if (selectedTab) {
+      selectedTab.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+      });
+    }
+  }, [value]);
+
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+
+    return color;
+  }
+
+  function stringAvatar(name) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: name.toUpperCase(),
+    };
+  }
+
+  function renderTabPanel(category) {
+    const filteredTasks = tasks.filter((task) => task.category === category);
+    return (
+      <TabPanel key={category} value={category}>
+        <List>
+          {filteredTasks.length > 0 ? (
+            filteredTasks.map((task, index) => (
+              <ListItem key={index}>
+                <Task task={task} />
+              </ListItem>
+            ))
+          ) : (
+            <ListItem>
+              <ListItemText primary="No tasks available" />
+            </ListItem>
+          )}
+        </List>
+      </TabPanel>
+    );
+  }
+
+  const categories = ["SP", "ED", "HE", "AR", "SS", "MD"];
 
   return (
-    <div className="taskbox">
-      <div className="task-avaiable">
-        <div className="task-avaiable-text">
-          {tasks.length > 0 ? (
-            <div>
-              {tasks.map((task, index) => (
-                <p key={index}>{`${task.name}: ${task.description}, ${task.duration}, ${task.exp}`}</p>
-              ))}
-            </div>
-          ) : (
-            <p>No tasks available</p>
-          )}
-        </div>
-      </div>
-    </div>
+    <Box sx={{ bgcolor: "white", borderRadius: "8px", maxWidth: "400px" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 0, borderColor: "divider" }}>
+          <TabList
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            variant="scrollable"
+            scrollButtons="false"
+            ref={tabListRef}
+          >
+            {categories.map((category) => (
+              <Tab key={category} label={category} value={category} />
+            ))}
+          </TabList>
+        </Box>
+        {categories.map((category) => renderTabPanel(category))}
+      </TabContext>
+    </Box>
   );
 }
