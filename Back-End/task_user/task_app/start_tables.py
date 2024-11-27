24a39695bf87b4ba5fb5ee9c0b9d5f6097c49c45
sim1gariglio <simone.gariglio1@gmail.com>
2024-11-27 06:02:29 -0500

    added ? button

--- Back-End/task_user/task_app/start_tables.py
+++ Back-End/task_user/task_app/start_tables.py
@@ -1,8 +1,8 @@
 
 from .models import Tasks
 from user_app.models import Avatars, Users
-from django.db.models.signals import post_migrate
-from django.dispatch import receiver
+from django.db.models.signals import post_migrate  # type: ignore
+from django.dispatch import receiver  # type: ignore
 
 def CreateTasks(**kwargs):
 	if not Avatars.objects.all():
@@ -42,32 +42,108 @@ def CreateTasks(**kwargs):
 		u.save()
 	if not Tasks.objects.all():
 		t = Tasks.objects.create(
-			author = Users.objects.get(id=3),
-			name = 'walk',
-			description = 'walk at least 10k step each day',
-			duration = '7 00:00:00',
-			exp = 1000,
-			category = 'SP'
-		)
+            author=Users.objects.get(id=3),
+            name='walk',
+            description='walk at least 10k step each day',
+            duration='7 00:00:00',
+            exp=1000,
+            category='SP'
+        )
 		t.save()
 		t = Tasks.objects.create(
-			author = Users.objects.get(id=3),
-			name = 'run',
-			description = 'improve your body health step by step',
-			duration = '30 00:00:00',
-			exp = 2000,
-			category = 'SP',
-			previous_task = Tasks.objects.get(id=1)
-		)
+            author=Users.objects.get(id=3),
+            name='run',
+            description='improve your body health step by step',
+            duration='30 00:00:00',
+            exp=2000,
+            category='SP',
+            previous_task=Tasks.objects.get(id=1)
+        )
 		t.save()
 		t = Tasks.objects.create(
-			author = Users.objects.get(id=3),
-			name = 'drink water',
-			description = 'drink 2l of water a day',
-			duration = '7 00:00:00',
-			exp = 500,
-			category = 'HE'
-		)
+            author=Users.objects.get(id=3),
+            name='drink water',
+            description='drink 2l of water a day',
+            duration='7 00:00:00',
+            exp=500,
+            category='HE'
+        )
+		t.save()
+		t = Tasks.objects.create(
+            author=Users.objects.get(id=3),
+            name='read a book',
+            description='read at least 30 pages of a book',
+            duration='7 00:00:00',
+            exp=800,
+            category='ED'
+        )
+		t.save()
+		t = Tasks.objects.create(
+            author=Users.objects.get(id=3),
+            name='write an essay',
+            description='write a 1000-word essay',
+            duration='14 00:00:00',
+            exp=1500,
+            category='ED',
+            previous_task=Tasks.objects.get(id=4)
+        )
+		t.save()
+		t = Tasks.objects.create(
+            author=Users.objects.get(id=3),
+            name='draw a sketch',
+            description='draw a sketch every day',
+            duration='7 00:00:00',
+            exp=700,
+            category='AR'
+        )
+		t.save()
+		t = Tasks.objects.create(
+            author=Users.objects.get(id=3),
+            name='paint a picture',
+            description='paint a picture',
+            duration='14 00:00:00',
+            exp=1400,
+            category='AR',
+            previous_task=Tasks.objects.get(id=6)
+        )
+		t.save()
+		t = Tasks.objects.create(
+            author=Users.objects.get(id=3),
+            name='volunteer',
+            description='volunteer at a local charity',
+            duration='7 00:00:00',
+            exp=900,
+            category='SS'
+        )
+		t.save()
+		t = Tasks.objects.create(
+            author=Users.objects.get(id=3),
+            name='organize an event',
+            description='organize a community event',
+            duration='30 00:00:00',
+            exp=1800,
+            category='SS',
+            previous_task=Tasks.objects.get(id=8)
+        )
+		t.save()
+		t = Tasks.objects.create(
+            author=Users.objects.get(id=3),
+            name='meditate',
+            description='meditate for 20 minutes',
+            duration='7 00:00:00',
+            exp=600,
+            category='MD'
+        )
+		t.save()
+		t = Tasks.objects.create(
+            author=Users.objects.get(id=3),
+            name='practice mindfulness',
+            description='practice mindfulness exercises',
+            duration='14 00:00:00',
+            exp=1200,
+            category='MD',
+            previous_task=Tasks.objects.get(id=10)
+        )
 		t.save()
 
 
--- Front-End/src/components/TaskAvaiable/TaskAvaiable.jsx
+++ Front-End/src/components/TaskAvaiable/TaskAvaiable.jsx
@@ -65,33 +65,6 @@ export default function TaskAvaiable() {
     }
   }, [value]);
 
-  function stringToColor(string) {
-    let hash = 0;
-    let i;
-
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = "#";
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.slice(-2);
-    }
-
-    return color;
-  }
-
-  function stringAvatar(name) {
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: name.toUpperCase(),
-    };
-  }
-
   function renderTabPanel(category) {
     const filteredTasks = tasks.filter((task) => task.category === category);
     return (
