761bf1d74c92cfc8e212f1ca529f5864003b71c4
Bombatomica64 <qualcosa2001@gmail.com>
2025-02-13 12:55:01 +0100

    new stuff

--- Back-End/pongProject/pong_app/signals.py
+++ Back-End/pongProject/pong_app/signals.py
@@ -1,13 +1,15 @@
 # praticamente quando un modello viene creato ricveve il segnale e fa quello che gli chiedi
+import random
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Game
 import logging
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+import time, asyncio
 
-#TODO implementa i logging schiavo, e' un solo file e' facile
-
+ring_size = [160 , 90]
+tick_rate = 30
 class GameState:
 	def __init__(self, player_1, player_2, game_id):
 		self.game_id = game_id
@@ -17,20 +19,29 @@ class GameState:
 		self.player_2_score = 0
 		self.player1_pos = 0
 		self.player2_pos = 0
-		self.ball_pos = 0
+		self.ball_pos = [0,0]
+		self.ball_vel = [0,0]
   
-	def movement(self, player, dir):
-		if player == self.player_1:
-			if dir == 1:
-				self.player1_pos += 1
-			else:
-				self.player1_pos -= 1
-		else:
-			if dir == 1:
-				self.player2_pos += 1
-			else:
-				self.player2_pos -= 1
-    
+	async def start(self):
+		self.running = True
+		tick_interval = 1 / tick_rate
+
+		ball_vel = [random.choice([-1, 1]), random.choice([-1, 1])]
+
+		while self.running:
+			start_time = time.monotonic()
+			
+			await self.physics()
+
+			await self.update()
+			
+			time.sleep(max(0, tick_interval - (time.monotonic() - start_time)))
+			await asyncio.sleep(tick_interval)
+	
+
+	def physics(self):
+
+
 	def update(self):
 		channel_layer = get_channel_layer()
 		try:
