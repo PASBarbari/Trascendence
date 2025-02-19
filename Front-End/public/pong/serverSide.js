15d190465f4bac3dc7708a004d0b023dcb9d9380
sim1gariglio <simone.gariglio1@gmail.com>
2025-02-19 05:00:21 -0500

    createGame

--- Front-End/public/pong/serverSide.js
+++ Front-End/public/pong/serverSide.js
@@ -1,22 +1,53 @@
 import { state } from "./state.js";
+import { getVariables } from "../var.js";
 
-export function initializeWebSocket() {
+//TODO aggiungere la lunghezza del player alle variabili e altre cose che vengono in mente
+export async function createGame(player1, player2) {
+	const { token } = getVariables();
+	
+	try {
+		const response = await fetch(`http://localhost:8004/pong/game?token=${token}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				player1: player1,
+				player2: player2,
+			}),
+	
+		});
+		if (response.ok) {	
+			const data = await response.json();
+			console.log("Game created:", data);
+			room_id = data.room_id;
+			initializeWebSocket(room_id);
+		} else {
+			const errorData = await response.json();
+			console.error("Errore nella risposta del server:", errorData);
+			//TODO Marco fai qualcosa per gestire l'errore
+		}
+	} catch (error) {
+		console.error("Errore nella richiesta:", error);
+		//TODO Marco fai qualcosa per gestire l'errore
+	}
+}
+
+export function initializeWebSocket(room_id) {
     const { token } = getVariables();
-    const wsUrl = `ws://127.0.0.1:8003/ws/user_notifications/?token=${token}`;
+    const wsUrl = `ws://127.0.0.1:8004/ws/pong/${room_id}/?token=${token}`;
 
     socket = new WebSocket(wsUrl);
 
     socket.onmessage = function (event) {
-        const message = JSON.parse(event.data);
+        const message = JSON.parse(event.data); 
 
-        console.log("/----websocket notification.js----\\");
         console.log("Nuovo messaggio:", message);
 
         if (message.type === "game_update") {
             updateGameState(message.data);
         }
 
-        console.log("\\____websocket notification.js____/");
     };
 
     socket.onopen = function () {
@@ -44,8 +75,8 @@ function updateGameState(data) {
     // Update other game state properties if needed
 }
 
-function getVariables() {
-    // Example implementation to retrieve the token from local storage or environment variables
-    const token = localStorage.getItem('token') || process.env.TOKEN;
-    return { token };
-}
+// function getVariables() {
+//     // Example implementation to retrieve the token from local storage or environment variables
+//     const token = localStorage.getItem('token') || process.env.TOKEN;
+//     return { token };
+// }
