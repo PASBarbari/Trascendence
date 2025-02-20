27993b4581a107f3f28daa1c5062cc10e9031165
sim1gariglio <simone.gariglio1@gmail.com>
2025-02-20 06:20:44 -0500

    worka

--- Back-End/pongProject/pong_app/consumers.py
+++ Back-End/pongProject/pong_app/consumers.py
@@ -1,7 +1,9 @@
+import asyncio
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 from .signals import GameState
+from .models import Game
 
 active_games = {}
 player_ready = {}
@@ -22,66 +24,88 @@ class GameTableConsumer(AsyncWebsocketConsumer):
 	async def disconnect(self, close_code):
 		pass
 
-	async def chat_message(self, event):
-		message = event['message']
+	async def receive(self, text_data):
+		data = json.loads(text_data)
+		message_type = data.get('type')
+		# print(f"message_type: {message_type}")
+		handler = self.message_handlers.get(message_type, self.default_handler)
+		await handler(self, data)
+
+	async def default_handler(self, data):
+		print(f"Unhandled message type: {data['type']}")
+
+	async def chat_message(self, data):
+		message = data['message']
 		await self.send(text_data=json.dumps({
 			'message': message
 		}))
 
-	async def start(self, event):
-		if self.room_id in active_games:
-			return
-		player_ready[self.room_id] = {event['player1']: False, event['player2']: False}
-		await self.send(text_data=json.dumps({
-			'message': 'Waiting for players to be ready...',
-			'ready': False
-		}))
-
-	async def player_ready(self, event):
-		player = event['player']
+	async def player_ready(self, data):
+		player = data['player']
+		if self.room_id not in player_ready:
+			player_ready[self.room_id] = [False, False]
 		player_ready[self.room_id][player] = True
-		if all(player_ready[self.room_id].values()):
+		print(player_ready[self.room_id])
+		if all(player_ready[self.room_id]):
 			await self.send(text_data=json.dumps({
 				'message': 'All players are ready!',
 				'ready': True
 			}))
-			await self.start_game(event)
+			# data['player1'] = await sync_to_async((Game.objects.get)(id=self.room_id).player_1)
+			# data['player2'] = await sync_to_async((Game.objects.get)(id=self.room_id).player_2)
+			game = await sync_to_async(Game.objects.get)(id=self.room_id)
+			data['player1'] = await sync_to_async(lambda: game.player_1)()
+			data['player2'] = await sync_to_async(lambda: game.player_2)()
+			await self.start_game(data)
 		else:
 			await self.send(text_data=json.dumps({
 				'message': 'Waiting for players to be ready...',
 				'ready': False
 			}))
 
-
-	async def start_game(self, event):
+	async def start_game(self, data):
 		if self.room_id in active_games:
 			return
 		del player_ready[self.room_id]
-		active_games[self.room_id] = GameState(event['player1'], event['player2'], self.room_id, event.get('player_length', 10))
-		await active_games[self.room_id].start()
+		active_games[self.room_id] = GameState(data['player1'], data['player2'], self.room_id, data.get('player_length', 10))
+		asyncio.create_task(active_games[self.room_id].start())
 
-	async def up(self, event, player):
-		active_games[self.room_id].up(event, player)
+	async def up(self, data):
+		player = data['player']
+		active_games[self.room_id].up(data, player)
 
-	async def down(self, event, player):
-		active_games[self.room_id].down(event, player)
+	async def down(self, data):
+		player = data['player']
+		active_games[self.room_id].down(data, player)
 
-	async def game_state(self, event):
-		self.send(text_data=json.dumps({
-			'game_state': event['game_state']
+	async def game_state(self, data):
+		print(data)
+		await self.send(text_data=json.dumps({
+			'game_state': data['game_state']
 		}))
 
-	async def game_over(self, event):
+	async def game_over(self, data):
 		await self.send(text_data=json.dumps({
 			'message': 'Game Over!',
 			'game_over': True
 		}))
 		del active_games[self.room_id]
-	
-	async def quit_game(self, event, player):
+		
+	async def quit_game(self, data):
+		player = data['player']
 		await self.send(text_data=json.dumps({
 			'message': f'Player {player} has quit the game!',
 			'game_over': True
 		}))
 		GameState.quit_game(player)
 		del active_games[self.room_id]
+
+	message_handlers = {
+		'chat_message': chat_message,
+		'player_ready': player_ready,
+		'up': up,
+		'down': down,
+		'game_state': game_state,
+		'game_over': game_over,
+		'quit_game': quit_game,
+	}
--- Front-End/public/pong/settings.js
+++ Front-End/public/pong/settings.js
@@ -4,89 +4,90 @@ import { createScore } from "./utils.js";
 import Stats from "three/addons/libs/stats.module.js";
 import * as UTILS from "./utils.js";
 import * as GAME from "./gameLogic.js";
+import { socket } from "./serverSide.js"; // Import the socket
 
 export {
-  showMainMenu,
-  shownbrOfPlayerMenu,
-  showSettingsMenu,
-  showPauseMenu,
-  hidePauseMenu,
-  startOnePlayerGame,
-  startTwoPlayerGame,
-  resumeGame,
-  exitGame,
-  saveSettings,
-  resetSettings,
-  startCrazymode,
-  startClassicmode,
+	showMainMenu,
+	shownbrOfPlayerMenu,
+	showSettingsMenu,
+	showPauseMenu,
+	hidePauseMenu,
+	startOnePlayerGame,
+	startTwoPlayerGame,
+	resumeGame,
+	exitGame,
+	saveSettings,
+	resetSettings,
+	startCrazymode,
+	startClassicmode,
 };
 
 function saveSettings() {
-  const player1Color = document.getElementById("player1Color").value;
-  const player1Emissive = document.getElementById("player1Emissive").value;
-  const player2Color = document.getElementById("player2Color").value;
-  const player2Emissive = document.getElementById("player2Emissive").value;
-  const ballColor = document.getElementById("ballColor").value;
-  const ballEmissive = document.getElementById("ballEmissive").value;
-  const ringColor = document.getElementById("ringColor").value;
-  const ringEmissive = document.getElementById("ringEmissive").value;
+	const player1Color = document.getElementById("player1Color").value;
+	const player1Emissive = document.getElementById("player1Emissive").value;
+	const player2Color = document.getElementById("player2Color").value;
+	const player2Emissive = document.getElementById("player2Emissive").value;
+	const ballColor = document.getElementById("ballColor").value;
+	const ballEmissive = document.getElementById("ballEmissive").value;
+	const ringColor = document.getElementById("ringColor").value;
+	const ringEmissive = document.getElementById("ringEmissive").value;
 
-  state.mat.p1.color.set(player1Color);
-  state.mat.p1.emissive.set(player1Emissive);
-  state.mat.p2.color.set(player2Color);
-  state.mat.p2.emissive.set(player2Emissive);
-  state.mat.ball.color.set(ballColor);
-  state.mat.ball.emissive.set(ballEmissive);
-  state.mat.ring.color.set(ringColor);
-  state.mat.ring.emissive.set(ringEmissive);
+	state.mat.p1.color.set(player1Color);
+	state.mat.p1.emissive.set(player1Emissive);
+	state.mat.p2.color.set(player2Color);
+	state.mat.p2.emissive.set(player2Emissive);
+	state.mat.ball.color.set(ballColor);
+	state.mat.ball.emissive.set(ballEmissive);
+	state.mat.ring.color.set(ringColor);
+	state.mat.ring.emissive.set(ringEmissive);
 
-  showMainMenu();
+	showMainMenu();
 }
 
 function resetSettings() {
-  document.getElementById("player1Color").value = "#4deeea";
-  document.getElementById("player1Emissive").value = "#4deeea";
-  document.getElementById("player2Color").value = "#ffe700";
-  document.getElementById("player2Emissive").value = "#ffe700";
-  document.getElementById("ballColor").value = "#0bff01";
-  document.getElementById("ballEmissive").value = "#0bff01";
-  document.getElementById("ringColor").value = "#ff0000";
-  document.getElementById("ringEmissive").value = "#0000ff";
+	document.getElementById("player1Color").value = "#4deeea";
+	document.getElementById("player1Emissive").value = "#4deeea";
+	document.getElementById("player2Color").value = "#ffe700";
+	document.getElementById("player2Emissive").value = "#ffe700";
+	document.getElementById("ballColor").value = "#0bff01";
+	document.getElementById("ballEmissive").value = "#0bff01";
+	document.getElementById("ringColor").value = "#ff0000";
+	document.getElementById("ringEmissive").value = "#0000ff";
 
-  state.mat.p1.color.set("#4deeea");
-  state.mat.p1.emissive.set("#4deeea");
-  state.mat.p2.color.set("#ffe700");
-  state.mat.p2.emissive.set("#ffe700");
-  state.mat.ball.color.set("#0bff01");
-  state.mat.ball.emissive.set("#0bff01");
-  state.mat.ring.color.set("#ff0000");
-  state.mat.ring.emissive.set("#0000ff");
+	state.mat.p1.color.set("#4deeea");
+	state.mat.p1.emissive.set("#4deeea");
+	state.mat.p2.color.set("#ffe700");
+	state.mat.p2.emissive.set("#ffe700");
+	state.mat.ball.color.set("#0bff01");
+	state.mat.ball.emissive.set("#0bff01");
+	state.mat.ring.color.set("#ff0000");
+	state.mat.ring.emissive.set("#0000ff");
 }
 
 function shownbrOfPlayerMenu() {
-  document.getElementById("menu").style.display = "none";
-  document.getElementById("modeMenu").style.display = "none";
-  document.getElementById("nbrOfPlayerMenu").style.display = "block";
+	document.getElementById("menu").style.display = "none";
+	document.getElementById("modeMenu").style.display = "none";
+	document.getElementById("nbrOfPlayerMenu").style.display = "block";
 }
 
 function showMainMenu() {
-  document.getElementById("nbrOfPlayerMenu").style.display = "none";
-  document.getElementById("settingsMenu").style.display = "none";
-  document.getElementById("pauseMenu").style.display = "none";
-  document.getElementById("menu").style.display = "block";
+	document.getElementById("nbrOfPlayerMenu").style.display = "none";
+	document.getElementById("settingsMenu").style.display = "none";
+	document.getElementById("pauseMenu").style.display = "none";
+	document.getElementById("menu").style.display = "block";
 }
 
 function showSettingsMenu() {
-  document.getElementById("menu").style.display = "none";
-  document.getElementById("settingsMenu").style.display = "block";
+	document.getElementById("menu").style.display = "none";
+	document.getElementById("settingsMenu").style.display = "block";
 }
 
 function showPauseMenu() {
-  document.getElementById("pauseMenu").style.display = "block";
+	document.getElementById("pauseMenu").style.display = "block";
 }
 
 function hidePauseMenu() {
-  document.getElementById("pauseMenu").style.display = "none";
+	document.getElementById("pauseMenu").style.display = "none";
 }
 
 function startOnePlayerGame() {
@@ -107,7 +108,19 @@ function startCrazymode() {
 	state.isStarted = true;
 	state.isPaused = false;
 	state.spawnPowerUpFlag = true;
-	GAME.serverAnimate();
+	// Send a message to the backend indicating that both players are ready
+	socket.send(
+		JSON.stringify({
+			type: "player_ready",
+			player: 0,
+		})
+	);
+	socket.send(
+		JSON.stringify({
+			type: "player_ready",
+			player: 1,
+		})
+	);
 }
 
 function startClassicmode() {
@@ -116,19 +129,34 @@ function startClassicmode() {
 	state.isStarted = true;
 	state.isPaused = false;
 	state.spawnPowerUpFlag = false;
-	GAME.serverAnimate();
+	// Send a message to the backend indicating that both players are ready
+	if (socket && socket.readyState === WebSocket.OPEN) {
+		console.log("Sending player_ready message to the server");
+		socket.send(
+			JSON.stringify({
+				type: "player_ready",
+				player: 0,
+			})
+		);
+		socket.send(
+			JSON.stringify({
+				type: "player_ready",
+				player: 1,
+			})
+		);
+	}
 }
 
 function resumeGame() {
-  hidePauseMenu();
-  state.isPaused = false;
-  GAME.serverAnimate();
+	hidePauseMenu();
+	state.isPaused = false;
+	GAME.serverAnimate();
 }
 
 function exitGame() {
-  state.isStarted = false;
-  state.isPaused = true;
-  state.IAisActive = false;
-  showMainMenu();
-  UTILS.restart_game();
+	state.isStarted = false;
+	state.isPaused = true;
+	state.IAisActive = false;
+	showMainMenu();
+	UTILS.restart_game();
 }
