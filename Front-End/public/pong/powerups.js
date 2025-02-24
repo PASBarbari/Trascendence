1348d16987d1d006ff0705f8835f7ffd234abfb6
sim1gariglio <simone.gariglio1@gmail.com>
2025-02-24 06:28:07 -0500

    cambiati i nomi alle variabili

--- Front-End/public/pong/powerups.js
+++ Front-End/public/pong/powerups.js
@@ -16,57 +16,61 @@ const powerUpTypes = [
 		color: 0x00ff00,
 		effect: () => {
 			state.scene.rotation.x += Math.PI;
-		}
+		},
 	},
 	{
 		type: "ringRotationY",
 		color: 0x00ffff,
 		effect: () => {
 			state.scene.rotation.y += Math.PI;
-		}
+		},
 	},
 	{
 		type: "ringRotationZ",
 		color: 0xffff00,
 		effect: () => {
 			state.scene.rotation.z += Math.PI;
-		}
+		},
 	},
 ];
-  
-  function spawnPowerUp() {
+
+function spawnPowerUp() {
 	const powerUpType =
-	  powerUpTypes[Math.floor(Math.random() * powerUpTypes.length)];
+		powerUpTypes[Math.floor(Math.random() * powerUpTypes.length)];
 	const geometry = new THREE.SphereGeometry(2, 32, 32);
-	const material = new THREE.MeshStandardMaterial({ color: powerUpType.color, emissive: powerUpType.color, emissiveIntensity: 0.5, metalness: 0, roughness: 0 });
+	const material = new THREE.MeshStandardMaterial({
+		color: powerUpType.color,
+		emissive: powerUpType.color,
+		emissiveIntensity: 0.5,
+		metalness: 0,
+		roughness: 0,
+	});
 	const powerUp = new THREE.Mesh(geometry, material);
-  
+
 	powerUp.position.set(
-	  ((Math.random() - 0.5) * state.ring.y) / 1.5,
-	  ((Math.random() - 0.5) * state.ring.x) / 1.5,
-	  0
+		((Math.random() - 0.5) * state.ring.heigth) / 1.5,
+		((Math.random() - 0.5) * state.ring.length) / 1.5,
+		0
 	);
-  
+
 	powerUp.userData = { type: powerUpType.type, effect: powerUpType.effect };
 	state.scene.add(powerUp);
 	state.powerUps.push(powerUp);
-  }
-  
-  
-  
-  function handlePowerUpCollision() {
+}
+
+function handlePowerUpCollision() {
 	state.powerUps.forEach((powerUp, index) => {
-	  if (UTILS.checkCollision(state.ball, powerUp)) {
-		// Handle the power-up effect here
-		console.log(`${powerUp.userData.type} power-up collected!`);
-		powerUp.userData.effect();
-		state.scene.remove(powerUp);
-		state.powerUps.splice(index, 1);
-	  }
+		if (UTILS.checkCollision(state.ball, powerUp)) {
+			// Handle the power-up effect here
+			console.log(`${powerUp.userData.type} power-up collected!`);
+			powerUp.userData.effect();
+			state.scene.remove(powerUp);
+			state.powerUps.splice(index, 1);
+		}
 	});
-  }
-  
-  function resetPowerUps() {
+}
+
+function resetPowerUps() {
 	// Reset player speed
 	state.player_speed = state.game.playerSpeed;
 	state.ball_speed = state.game.ballSpeed;
@@ -85,5 +89,4 @@ const powerUpTypes = [
 	state.r_right.material.color.set(state.game.ringColor);
 	state.r_right.material.emissive.set(state.game.ringEmissive);
 	state.scene.rotation.set(0, 0, 0);
-	}
-  
+}
--- Front-End/public/pong/utils.js
+++ Front-End/public/pong/utils.js
@@ -1,31 +1,38 @@
-import * as THREE from 'three';
-import { state } from './state.js';
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import * as UP from './powerups.js';
-import * as SETTINGS from './settings.js';
-import * as SETUP from './setup.js';
-
+import * as THREE from "three";
+import { state } from "./state.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+import * as UP from "./powerups.js";
+import * as SETTINGS from "./settings.js";
+import * as SETUP from "./setup.js";
 
 export function createScore() {
 	const loader = new FontLoader();
 	const fonts = loader.load(
 		// resource URL
-		'https://threejs.org/examples/fonts/helvetiker_regular.typeface.json',
+		"https://threejs.org/examples/fonts/helvetiker_regular.typeface.json",
 
 		// onLoad callback
 		function (font) {
 			// do something with the font
-			const geometry = new TextGeometry(state.p1_score + ' : ' + state.p2_score, {
-				font: font,
-				size: 10,
-				depth: 1,
-				curveSegments: 12,
-			});
+			const geometry = new TextGeometry(
+				state.p1_score + " : " + state.p2_score,
+				{
+					font: font,
+					size: 10,
+					depth: 1,
+					curveSegments: 12,
+				}
+			);
 			geometry.computeBoundingBox();
-			const centerOffset = -(geometry.boundingBox.max.x - geometry.boundingBox.min.x) / 2;
+			const centerOffset =
+				-(geometry.boundingBox.max.x - geometry.boundingBox.min.x) / 2;
 			state.scoreText = new THREE.Mesh(geometry, state.mat.score);
-			state.scoreText.position.set(centerOffset, state.ring.y / 3, 0);
+			state.scoreText.position.set(
+				centerOffset,
+				state.ring.heigth / 3,
+				0
+			);
 			state.scene.add(state.scoreText);
 		},
 
@@ -36,7 +43,7 @@ export function createScore() {
 
 		// onError callback
 		function (err) {
-			console.log('An error happened');
+			console.log("An error happened");
 		}
 	);
 }
@@ -49,118 +56,122 @@ export function updateScore() {
 }
 
 export function toggleStats(show) {
-	state.stats.dom.style.display = show ? 'block' : 'none';
+	state.stats.dom.style.display = show ? "block" : "none";
 }
 
 export function p1IsHit() {
 	if (
-	  state.ball.position.x - state.ball_radius - state.ball_speed <=
-		state.p1.position.x + state.player.h / 2 &&
-	  state.ball.position.x - state.ball_speed >
-		state.p1.position.x - state.player.h / 2 &&
-	  state.ball.position.y - state.ball_radius <=
-		state.p1.position.y + state.player.y / 2 &&
-	  state.ball.position.y + state.ball_radius >=
-		state.p1.position.y - state.player.y / 2
+		state.ball.position.x - state.ball_radius - state.ball_speed <=
+			state.p1.position.x + state.player.width / 2 &&
+		state.ball.position.x - state.ball_speed >
+			state.p1.position.x - state.player.width / 2 &&
+		state.ball.position.y - state.ball_radius <=
+			state.p1.position.y + state.player.length / 2 &&
+		state.ball.position.y + state.ball_radius >=
+			state.p1.position.y - state.player.length / 2
 	)
-	  return true;
+		return true;
 	return false;
-  }
+}
 
 export function p2IsHit() {
-  if (
-	state.ball.position.x + state.ball_radius + state.ball_speed >=
-	  state.p2.position.x - state.player.h / 2 &&
-	state.ball.position.x + state.ball_speed <
-	  state.p2.position.x + state.player.h / 2 &&
-	state.ball.position.y - state.ball_radius <=
-	  state.p2.position.y + state.player.y / 2 &&
-	state.ball.position.y + state.ball_radius >=
-	  state.p2.position.y - state.player.y / 2
-  )
-	return true;
-  return false;
+	if (
+		state.ball.position.x + state.ball_radius + state.ball_speed >=
+			state.p2.position.x - state.player.width / 2 &&
+		state.ball.position.x + state.ball_speed <
+			state.p2.position.x + state.player.width / 2 &&
+		state.ball.position.y - state.ball_radius <=
+			state.p2.position.y + state.player.length / 2 &&
+		state.ball.position.y + state.ball_radius >=
+			state.p2.position.y - state.player.length / 2
+	)
+		return true;
+	return false;
 }
 
 export function score() {
 	state.wallHitPosition = 0;
 	updateScore();
 	state.ball.position.set(0, 0, 0);
-	state.ball_speed = state.ring.y / 150;
+	state.ball_speed = state.ring.heigth / 150;
 	state.angle = Math.floor(Math.random() * 70);
 	if (state.angle % 2) state.angle *= -1;
 	if (state.angle % 3) state.angle += 180;
-  
+
 	if (state.p1_score >= state.maxScore || state.p2_score >= state.maxScore) {
-	  game_over();
+		game_over();
 	}
-  
+
 	UP.resetPowerUps();
-  }
+}
 
 export function checkCollision(ball, powerUp) {
 	if (
-	  state.ball.position.x - state.ball_radius <= powerUp.position.x + 1.25 &&
-	  state.ball.position.x + state.ball_radius >= powerUp.position.x - 1.25 &&
-	  state.ball.position.y - state.ball_radius <= powerUp.position.y + 1.25 &&
-	  state.ball.position.y + state.ball_radius >= powerUp.position.y - 1.25
+		state.ball.position.x - state.ball_radius <=
+			powerUp.position.x + 1.25 &&
+		state.ball.position.x + state.ball_radius >=
+			powerUp.position.x - 1.25 &&
+		state.ball.position.y - state.ball_radius <=
+			powerUp.position.y + 1.25 &&
+		state.ball.position.y + state.ball_radius >= powerUp.position.y - 1.25
 	)
-	  return true;
-  }
+		return true;
+}
 
 export function createWinnerText(winner) {
 	const loader = new FontLoader();
 	loader.load(
-	  "https://threejs.org/examples/fonts/helvetiker_regular.typeface.json",
-	  function (font) {
-		const geometry = new TextGeometry(`${winner} Wins!`, {
-		  font: font,
-		  size: 10,
-		  depth: 1,
-		  curveSegments: 12,
-		  bevelEnabled: false,
-		});
-		geometry.computeBoundingBox();
-		const centerOffset =
-		  -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
-		state.winnerText = new THREE.Mesh(geometry, state.mat.score);
-		state.winnerText.position.set(centerOffset, 20, 0);
-		state.scene.add(state.winnerText);
-		state.renderer.render(state.scene, state.camera);
-	  }
+		"https://threejs.org/examples/fonts/helvetiker_regular.typeface.json",
+		function (font) {
+			const geometry = new TextGeometry(`${winner} Wins!`, {
+				font: font,
+				size: 10,
+				depth: 1,
+				curveSegments: 12,
+				bevelEnabled: false,
+			});
+			geometry.computeBoundingBox();
+			const centerOffset =
+				-0.5 *
+				(geometry.boundingBox.max.x - geometry.boundingBox.min.x);
+			state.winnerText = new THREE.Mesh(geometry, state.mat.score);
+			state.winnerText.position.set(centerOffset, 20, 0);
+			state.scene.add(state.winnerText);
+			state.renderer.render(state.scene, state.camera);
+		}
 	);
-  }
+}
 
-export  function removeWinnerText() {
+export function removeWinnerText() {
 	if (state.winnerText) {
-	  state.scene.remove(state.winnerText);
+		state.scene.remove(state.winnerText);
 	}
-  }
+}
 
 export function restart_game() {
-  state.p1_score = 0;
-  state.p2_score = 0;
-  state.p2_move_y = 0;
-  state.p1_move_y = 0;
-  state.wallHitPosition = 0;
-  state.lastPowerUpSpawnTime = 0;
-  removeWinnerText();
-  updateScore();
-  state.ball.position.set(0, 0, 0);
-  state.ball_speed = state.ring.y / 150;
-  state.p1.position.set(-((state.ring.y * 2) / 5), 0, 0);
-  state.p2.position.set((state.ring.y * 2) / 5, 0, 0);
-  state.angle = Math.floor(Math.random() * 70);
-  if (state.angle % 2) state.angle *= -1;
-  if (state.angle % 3) state.angle += 180;
-  state.camera.position.set(state.cam.x, state.cam.y, state.cam.z);
-  state.camera.lookAt(state.look.x, state.look.y, state.look.z);
-  if (state.powerUps.length > 0) {
-	state.powerUps.forEach((powerUp) => state.scene.remove(powerUp));
-	state.powerUps = [];
-  }
-  state.scene.rotation.set(0, 0, 0);
-  SETUP.initGame();
+	state.p1_score = 0;
+	state.p2_score = 0;
+	state.p2_move_y = 0;
+	state.p1_move_y = 0;
+	state.wallHitPosition = 0;
+	state.lastPowerUpSpawnTime = 0;
+	removeWinnerText();
+	updateScore();
+	state.ball.position.set(0, 0, 0);
+	state.ball_speed = state.ring.heigth / 150;
+	state.p1.position.set(-((state.ring.heigth * 2) / 5), 0, 0);
+	state.p2.position.set((state.ring.heigth * 2) / 5, 0, 0);
+	state.angle = Math.floor(Math.random() * 70);
+	if (state.angle % 2) state.angle *= -1;
+	if (state.angle % 3) state.angle += 180;
+	state.camera.position.set(state.cam.x, state.cam.y, state.cam.z);
+	state.camera.lookAt(state.look.x, state.look.y, state.look.z);
+	if (state.powerUps.length > 0) {
+		state.powerUps.forEach((powerUp) => state.scene.remove(powerUp));
+		state.powerUps = [];
+	}
+	state.scene.rotation.set(0, 0, 0);
+	SETUP.initGame();
 }
 
 export function game_over() {
