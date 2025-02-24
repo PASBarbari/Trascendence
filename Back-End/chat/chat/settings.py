b913922ca03a1d886781eeb6c82cf603f7d82396
Bombatomica64 <qualcosa2001@gmail.com>
2025-02-24 11:29:30 +0100

    start

--- Back-End/chat/chat/settings.py
+++ Back-End/chat/chat/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'rest_framework',
+	'rest_framework_simplejwt',
 	'my_chat',
 	'oauth2_provider',
 	'django_filters',
