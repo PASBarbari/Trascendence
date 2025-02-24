acc32fc1ec4e17b3e7c2f09995d291bdddc7c2ab
sim1gariglio <simone.gariglio1@gmail.com>
2025-02-24 06:28:07 -0500

    cambiati i nomi alle variabili

--- Front-End/public/pong/gameLogic.js
+++ Front-End/public/pong/gameLogic.js
@@ -25,33 +25,41 @@ export const animate = (timestamp) => {
 				state.hit_position =
 					state.ball.position.y - state.p1.position.y;
 				state.wallHitPosition = 0;
-				state.angle = (state.hit_position / state.player.y) * -90;
-				if (state.ball_speed < 5 * state.player.h)
+				state.angle = (state.hit_position / state.player.length) * -90;
+				if (state.ball_speed < 5 * state.player.width)
 					state.ball_speed += 0.1;
 			} else if (UTILS.p2IsHit()) {
 				state.hit_position =
 					state.ball.position.y - state.p2.position.y;
 				state.wallHitPosition = 0;
-				state.angle = 180 + (state.hit_position / state.player.y) * 90;
-				if (state.ball_speed < 5 * state.player.h)
+				state.angle =
+					180 + (state.hit_position / state.player.length) * 90;
+				if (state.ball_speed < 5 * state.player.width)
 					state.ball_speed += 0.1;
 			} else if (
 				(state.wallHitPosition <= 0 &&
-					state.ball.position.y + state.ball_radius + state.ball_speed >= state.ring.x / 2) ||
-				(state.wallHitPosition >= 0 &&state.ball.position.y - state.ball_radius - state.ball_speed <= -state.ring.x / 2)
+					state.ball.position.y +
+						state.ball_radius +
+						state.ball_speed >=
+						state.ring.length / 2) ||
+				(state.wallHitPosition >= 0 &&
+					state.ball.position.y -
+						state.ball_radius -
+						state.ball_speed <=
+						-state.ring.length / 2)
 			) {
 				state.wallHitPosition = state.ball.position.y;
 				state.angle *= -1;
 			} else if (
 				state.ball.position.x - state.ball_radius <
-				state.r_left.position.x + state.ring.h
+				state.r_left.position.x + state.ring.thickness
 			) {
 				console.log("p2 ha segnato");
 				state.p2_score += 1;
 				UTILS.score();
 			} else if (
 				state.ball.position.x + state.ball_radius >
-				state.r_right.position.x - state.ring.h
+				state.r_right.position.x - state.ring.thickness
 			) {
 				console.log("p1 ha segnato");
 				state.p1_score += 1;
@@ -63,21 +71,21 @@ export const animate = (timestamp) => {
 				state.ball_speed * Math.cos((state.angle * Math.PI) / 180);
 			if (
 				(state.p1_move_y > 0 &&
-					state.p1.position.y + state.player.y / 2 <=
-						state.ring.x / 2 - state.ring.h / 2) ||
+					state.p1.position.y + state.player.length / 2 <=
+						state.ring.length / 2 - state.ring.thickness / 2) ||
 				(state.p1_move_y < 0 &&
-					state.p1.position.y - state.player.y / 2 >=
-						-state.ring.x / 2 + state.ring.h / 2)
+					state.p1.position.y - state.player.length / 2 >=
+						-state.ring.length / 2 + state.ring.thickness / 2)
 			)
 				state.p1.position.y += state.p1_move_y;
 			if (state.IAisActive) IA.moveIA();
 			if (
 				(state.p2_move_y > 0 &&
-					state.p2.position.y + state.player.y / 2 <=
-						state.ring.x / 2 - state.ring.h / 2) ||
+					state.p2.position.y + state.player.length / 2 <=
+						state.ring.length / 2 - state.ring.thickness / 2) ||
 				(state.p2_move_y < 0 &&
-					state.p2.position.y - state.player.y / 2 >=
-						-state.ring.x / 2 + state.ring.h / 2)
+					state.p2.position.y - state.player.length / 2 >=
+						-state.ring.length / 2 + state.ring.thickness / 2)
 			)
 				state.p2.position.y += state.p2_move_y;
 			if (
@@ -100,5 +108,5 @@ export const animate = (timestamp) => {
 };
 
 export const serverAnimate = () => {
-	state.renderer.render(state.scene, state.camera)
+	state.renderer.render(state.scene, state.camera);
 };
--- Front-End/public/pong/setup.js
+++ Front-End/public/pong/setup.js
@@ -1,15 +1,19 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import { state } from "./state.js";
 import { createScore, restart_game } from "./utils.js";
-import Stats from 'three/addons/libs/stats.module.js';
+import Stats from "three/addons/libs/stats.module.js";
 import * as UTILS from "./utils.js";
 
 //Scene setup
 
-export function setupGame(){
-
+export function setupGame() {
 	state.scene = new THREE.Scene();
-	state.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	state.camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
 	state.camera.position.set(state.cam.x, state.cam.y, state.cam.z);
 	state.camera.lookAt(state.look.x, state.look.y, state.look.z);
 
@@ -17,35 +21,92 @@ export function setupGame(){
 	state.renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(state.renderer.domElement);
 
-
 	//Ring setup
 
-	state.r_bottom = new THREE.Mesh(new THREE.BoxGeometry(state.ring.y, state.ring.h, state.ring.z), state.mat.ring);
-	state.r_top = new THREE.Mesh(new THREE.BoxGeometry(state.ring.y, state.ring.h, state.ring.z), state.mat.ring);
-	state.r_left = new THREE.Mesh(new THREE.BoxGeometry(state.ring.h, state.ring.x, state.ring.z), state.mat.ring);
-	state.r_right = new THREE.Mesh(new THREE.BoxGeometry(state.ring.h, state.ring.x, state.ring.z), state.mat.ring);
+	state.r_bottom = new THREE.Mesh(
+		new THREE.BoxGeometry(
+			state.ring.heigth,
+			state.ring.thickness,
+			state.ring.width
+		),
+		state.mat.ring
+	);
+	state.r_top = new THREE.Mesh(
+		new THREE.BoxGeometry(
+			state.ring.heigth,
+			state.ring.thickness,
+			state.ring.width
+		),
+		state.mat.ring
+	);
+	state.r_left = new THREE.Mesh(
+		new THREE.BoxGeometry(
+			state.ring.thickness,
+			state.ring.length,
+			state.ring.width
+		),
+		state.mat.ring
+	);
+	state.r_right = new THREE.Mesh(
+		new THREE.BoxGeometry(
+			state.ring.thickness,
+			state.ring.length,
+			state.ring.width
+		),
+		state.mat.ring
+	);
 
-
-	state.r_bottom.position.set(0, -((state.ring.x + state.ring.h) / 2), 0);
-	state.r_top.position.set(0, ((state.ring.x + state.ring.h) / 2), 0);
-	state.r_left.position.set(-((state.ring.y - state.ring.h) / 2), 0, 0);
-	state.r_right.position.set(((state.ring.y - state.ring.h) / 2), 0, 0);
+	state.r_bottom.position.set(
+		0,
+		-((state.ring.length + state.ring.thickness) / 2),
+		0
+	);
+	state.r_top.position.set(
+		0,
+		(state.ring.length + state.ring.thickness) / 2,
+		0
+	);
+	state.r_left.position.set(
+		-((state.ring.heigth - state.ring.thickness) / 2),
+		0,
+		0
+	);
+	state.r_right.position.set(
+		(state.ring.heigth - state.ring.thickness) / 2,
+		0,
+		0
+	);
 	state.ring3D = new THREE.Group();
 	state.ring3D.add(state.r_bottom, state.r_top, state.r_left, state.r_right);
 
-
 	//Players setup
 
-	state.p1 = new THREE.Mesh(new THREE.BoxGeometry(state.player.h, state.player.y, state.player.z), state.mat.p1);
-	state.p2 = new THREE.Mesh(new THREE.BoxGeometry(state.player.h, state.player.y, state.player.z), state.mat.p2);
-	state.p1.position.set(-(state.ring.y * 2 / 5), 0, 0);
-	state.p2.position.set((state.ring.y * 2 / 5), 0, 0);
+	state.p1 = new THREE.Mesh(
+		new THREE.BoxGeometry(
+			state.player.width,
+			state.player.length,
+			state.player.height
+		),
+		state.mat.p1
+	);
+	state.p2 = new THREE.Mesh(
+		new THREE.BoxGeometry(
+			state.player.width,
+			state.player.length,
+			state.player.height
+		),
+		state.mat.p2
+	);
+	state.p1.position.set(-((state.ring.heigth * 2) / 5), 0, 0);
+	state.p2.position.set((state.ring.heigth * 2) / 5, 0, 0);
 
 	//Ball setup
 
-	state.ball = new THREE.Mesh(new THREE.SphereGeometry(state.ball_radius), state.mat.ball);
+	state.ball = new THREE.Mesh(
+		new THREE.SphereGeometry(state.ball_radius),
+		state.mat.ball
+	);
 	state.ball.position.set(0, 0, 0);
-
 
 	//Light setup
 
@@ -59,7 +120,7 @@ export function setupGame(){
 	state.stats = new Stats();
 	state.stats.showPanel(0);
 	document.body.appendChild(state.stats.dom);
-	state.stats.dom.style.display = 'none'; // Hide stats by default
+	state.stats.dom.style.display = "none"; // Hide stats by default
 
 	//Game setup
 
@@ -72,7 +133,7 @@ export function setupGame(){
 	// UTILS.restart_game();
 }
 
-export function initGame(){
+export function initGame() {
 	state.game.ballColor = state.mat.ball.color.getHex();
 	state.game.player1Color = state.mat.p1.color.getHex();
 	state.game.player1Emissive = state.mat.p1.emissive.getHex();
