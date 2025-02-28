de567ae4d8b9723093ad0c70a6272cadb3d3cd7c
Bombatomica64 <qualcosa2001@gmail.com>
2025-02-28 10:32:48 +0100

    Update PostgreSQL storage size, add new namespaces and configmaps, and enhance entrypoint scripts for multiple services

--- Back-End/chat/entrypoint.sh
+++ Back-End/chat/entrypoint.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-cd /chat
+# Run migrations at startup
+python manage.py makemigrations
+python manage.py makemigrations my_chat
+python manage.py migrate
 
-# Apply datab
-python3 manage.py makemigrations
-python3 manage.py makemigrations my_chat
-python3 manage.py migrate
-
-# Star
-python3 manage.py runserver 0.0.0.0:8000
+# For development
+if [ "$DEBUG" = True ]; then
+    python manage.py runserver 0.0.0.0:8000 || { echo "Runserver failed to start"; exit 1; }
+else
+    # For production use Daphne (ASGI server)
+    daphne -b 0.0.0.0 -p 8000 chat.asgi:application || { echo "Daphne failed to start"; exit 1; }
+fi
