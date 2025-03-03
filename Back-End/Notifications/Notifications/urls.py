188c465a7a9c04920c66fea38e1bcd6e486dc25e
Bombatomica64 <qualcosa2001@gmail.com>
2025-03-03 10:09:41 +0100

    Enhance settings.py to ensure URL scheme for microservices, update database configuration with environment variables, and add superuser creation on migration.

--- Back-End/Notifications/Notifications/urls.py
+++ Back-End/Notifications/Notifications/urls.py
@@ -19,5 +19,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-		path('notification/', include('my_notifications.urls'))
+    path('notification/', include('my_notifications.urls')),
 ]
