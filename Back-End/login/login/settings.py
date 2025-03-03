a2e3774e532d263b400857e416305190a653c0f1
marcor2742 <marcoruggiero.mail@gmail.com>
2025-03-03 12:45:27 +0100

    Aggiungi FORCE_SCRIPT_NAME per le API nei file settings.py e rinomina il servizio di login; aggiorna lo script di avvio per rimuovere pacchetti non necessari e abilitare l'ingresso in Minikube.

--- Back-End/login/login/settings.py
+++ Back-End/login/login/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 import os
 import secrets
 
+# Aggiungi queste impostazioni
+FORCE_SCRIPT_NAME = '/api/login'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
