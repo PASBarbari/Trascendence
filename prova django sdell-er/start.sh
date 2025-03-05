741e96754e7c49548eff1e6d250cabebbb581076
SamyBravy <samybravy@gmail.com>
2025-03-05 14:34:41 +0100

    Add OAuth2 support and configure user authentication in Django project

---
+++ prova django sdell-er/start.sh
@@ -0,0 +1,6 @@
+python3 -m venv venv && \
+source venv/bin/activate && \
+pip install django && \
+pip install django-oauth-toolkit && \
+export ID=5qg6mPjfIeJ6ArwbSFvpYcjdygrtix97KzxmIq1R && \
+export SECRET=aBfs9aY2xqcAmmW31QDUFcVfPyud8qe6reAY09CWRuoHIbsnQsKz7b7NpaD4Hc5CmRwI7YwxFuG3EPDagSwCN8jxLj98fIyoRFCpcBPWd61ilPUuCe8DTHBz7GDwKDzc
